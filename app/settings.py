12e8776c06ad0fe3296fc60549f98a2a4cf430d2
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-09 22:21:22 +0100

    refactor cors

--- app/settings.py
+++ app/settings.py
@@ -63,12 +63,13 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-#CORS_ALLOW_ALL_ORIGINS = True
+# since security is not important
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
+#CORS_ALLOWED_ORIGINS = [
    # "https://yourfrontenddomain.com",
-    "http://localhost:3000",  
-]
+ #   "http://localhost:3000",  
+#]
 
 
 
