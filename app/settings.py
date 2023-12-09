98bd44205a50f1bb848f326027b18b0aa013521b
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-09 15:45:42 +0100

    add cors

--- app/settings.py
+++ app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'usersApp',
     'articlesApp',
     'clientsApp'
@@ -59,7 +60,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+#CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+   # "https://yourfrontenddomain.com",
+    "http://localhost:3000",  
+]
+
+
 
 ROOT_URLCONF = 'app.urls'
 
