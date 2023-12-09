17c52dca51d9bdc580826b449e279d726f48204d
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-09 12:22:27 +0100

    articlesApp basic CRUD is ok

---
+++ articlesApp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Article
+from .serializers import ArticleSerializer
+
+# Create your views here.
+class ArticleList(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetail(APIView):
+    def get_object(self, article_id):
+        return get_object_or_404(Article, id=article_id)
+
+    def get(self, request, article_id):
+        article = self.get_object(article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, article_id):
+        article = self.get_object(article_id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, article_id):
+        article = self.get_object(article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
