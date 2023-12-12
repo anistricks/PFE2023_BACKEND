8fc64fa4a63fbea79b2e8b45fca6be951a0a404c
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-12 21:54:45 +0100

    test pipeline

--- app/settings.py
+++ app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
     'usersApp',
     'articlesApp',
@@ -162,11 +163,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
+#REST_FRAMEWORK = {
+   # 'DEFAULT_AUTHENTICATION_CLASSES': (
+       # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+ #   ),
+#}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
