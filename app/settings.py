614e28c7e8249b13b4bd47e81f63a8d27c625bd0
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-12 18:11:11 +0100

    test for pipelines

--- app/settings.py
+++ app/settings.py
@@ -105,7 +105,6 @@ test_true = "manage.py" in sys.argv and "test" in sys.argv
 if test_true:
     DATABASES = {
         'default': {
-            'SECRET_KEY' : os.environ.get('SECRET_KEY'),
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
