971f27a7bb677e9aa9cd3bff2cd5baf3e90bcf88
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-12 18:02:53 +0100

    comment to debug

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
+#from django.test import TestCase
 
 #  Simple test
-class SimpleTest(TestCase):
-    def test_yes_equals_yes(self):
-        self.assertEqual('yes', 'yes')
+#class SimpleTest(TestCase):
+   # def test_yes_equals_yes(self):
+   #     self.assertEqual('yes', 'yes')
