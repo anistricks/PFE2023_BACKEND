937911ff3d28a65ce90f2d772e24afb51ae703c4
Assaidi Anis <anis.assaidi@student.vinci.be>
2023-12-12 21:41:40 +0100

    test pipeline

--- app/settings.py
+++ app/settings.py
@@ -14,20 +14,7 @@ from pathlib import Path
 import os
 import sys
 from datetime  import timedelta
-#test pipeline
-print("Before loading :",  os.environ.get('SECRET_KEY'))
-print("Before loading :",  os.environ.get('SECRET_KEY'))
-print("Before loading :",  os.environ.get('SECRET_KEY'))
-if os.getenv('SECRET_KEY') is None:
-    
-    load_dotenv()
-
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-print("After loading :",  os.environ.get('SECRET_KEY'))
-print("After loading :",  os.environ.get('SECRET_KEY'))
-print("After loading :",  os.environ.get('SECRET_KEY'))
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get('SECRET_KEY') 
+SECRET_KEY =os.environ('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
