4b8334cf8d2dc450cbc79bd00947077c0e17d418
maxence.vangrunderbeec <maxence.vangrunderbeec@student.vinci.be>
2023-12-13 13:22:32 +0100

    add delete by client

--- commandesApp/urls.py
+++ commandesApp/urls.py
@@ -1,6 +1,6 @@
 # commandsApp/urls.py
 from django.urls import path
-from .views import CommandeList, CommandeDetail, LigneCommandeList, LigneCommandeDetail,ArticlesByCommandeList
+from .views import CommandeList, CommandeDetail, LigneCommandeList, LigneCommandeDetail,ArticlesByCommandeList, CommandeClient
 
 urlpatterns = [
     path('', CommandeList.as_view(), name='commande-list'),
@@ -8,5 +8,5 @@ urlpatterns = [
     path('lignes_commande/', LigneCommandeList.as_view(), name='ligne-commande-list'),
     path('lignes_commande/<int:ligne_commande_id>/', LigneCommandeDetail.as_view(), name='ligne-commande-detail'),
     path('<int:commande_id>/articles/', ArticlesByCommandeList.as_view(), name='articles-by-commande-list'),
- 
+    path('client/<int:client_id>/', CommandeClient.as_view(), name='commande-detail'),
 ]
